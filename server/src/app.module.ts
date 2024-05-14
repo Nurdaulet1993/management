3c133bf8b849ec3f3d2842adebfa48269b0e3ede
Nurdaulet <nurik93sat@gmail.com>
2024-05-14 18:21:18 +0500

    Config

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -3,12 +3,13 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import * as process from 'process';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
-      envFilePath: `.env.development`,
+      envFilePath: `.env.${process.env.NODE_ENV}`
     }),
     TypeOrmModule.forRootAsync({
       inject: [ConfigService],
