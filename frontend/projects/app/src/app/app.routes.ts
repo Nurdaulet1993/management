b4323305acfefab205191406c8e5ab19950188a7
Nurdaulet <nurik93sat@gmail.com>
2024-05-21 02:42:39 +0500

    Auth components and routes

--- frontend/projects/app/src/app/app.routes.ts
+++ frontend/projects/app/src/app/app.routes.ts
@@ -1,3 +1,22 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'home',
+    loadComponent: () => import('./home/home.component').then(c => c.HomeComponent)
+  },
+  {
+    path: 'auth',
+    loadChildren: () => import('./auth/auth.routes').then(r => r.AUTH_ROUTES)
+  },
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
+    path: '**',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  }
+];
---
+++ frontend/projects/app/src/app/auth/auth.routes.ts
@@ -0,0 +1,19 @@
+import { Routes } from '@angular/router';
+import { SignInComponent } from './sign-in/sign-in.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
+
+export const AUTH_ROUTES: Routes = [
+  {
+    path: 'sign-in',
+    component: SignInComponent
+  },
+  {
+    path: 'sign-up',
+    component: SignUpComponent
+  },
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'sign-in'
+  }
+]
