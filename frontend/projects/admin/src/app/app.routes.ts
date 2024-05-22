33b39c16846b5f0213828fda807447999cd2aa82
Nurdaulet <nurik93sat@gmail.com>
2024-05-23 03:23:52 +0500

    Dashboard page

--- frontend/projects/admin/src/app/app.routes.ts
+++ frontend/projects/admin/src/app/app.routes.ts
@@ -4,5 +4,14 @@ export const routes: Routes = [
   {
     path: 'auth',
     loadChildren: () => import('./auth/auth.routes').then(r => r.AUTH_ROUTES)
+  },
+  {
+    path: '',
+    loadComponent: () => import('./dashboard/dashboard.component').then(c => c.DashboardComponent)
+  },
+  {
+    path: '**',
+    pathMatch: 'full',
+    redirectTo: ''
   }
 ];
