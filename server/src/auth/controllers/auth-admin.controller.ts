0f4edf5b2d83deee109e9166675e1feb3514efc9
Nurdaulet <nurik93sat@gmail.com>
2024-05-23 04:02:36 +0500

    Admin login

--- server/src/auth/controllers/auth-admin.controller.ts
+++ server/src/auth/controllers/auth-admin.controller.ts
@@ -1,7 +1,8 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, Request, UseGuards } from '@nestjs/common';
 import { Public } from '../public.decorator';
 import { AuthService } from '../auth.service';
 import { RegisterDto } from '../dto/register.dto';
+import { LocalAuthGuard } from '../guards/local-auth.guard';
 
 @Controller('admin/auth')
 export class AuthAdminController {
@@ -14,4 +15,13 @@ export class AuthAdminController {
   signUp(@Body() body: RegisterDto) {
     return this.authService.signUp(body, true);
   }
+
+
+  @Public()
+  @UseGuards(LocalAuthGuard)
+  @Post('login')
+  async login(@Request() req: any) {
+    return this.authService.login(req.user)
+  }
+
 }
