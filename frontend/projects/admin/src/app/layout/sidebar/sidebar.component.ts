722717d173e1ee349990a6cad159ce17da15810a
Nurdaulet <nurik93sat@gmail.com>
2024-05-25 07:11:21 +0500

    Navigation simple

--- frontend/projects/admin/src/app/layout/sidebar/sidebar.component.ts
+++ frontend/projects/admin/src/app/layout/sidebar/sidebar.component.ts
@@ -1,10 +1,13 @@
 import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { LayoutService } from '../layout.service';
+import {RouterLink} from '@angular/router';
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [],
+  imports: [
+    RouterLink
+  ],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush
