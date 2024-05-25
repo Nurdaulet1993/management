7abf31c86baadcadd67674576209a725c44c06d2
Nurdaulet <nurik93sat@gmail.com>
2024-05-25 05:06:07 +0500

    Layout start

--- frontend/projects/admin/src/app/app.component.ts
+++ frontend/projects/admin/src/app/app.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { LayoutModule } from './layout/layout.module';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, LayoutModule],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
